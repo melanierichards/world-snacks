f95d66e2c3b6e5bd79698222bceb0541d5fb8909
Melanie Richards <mr@melanie-richards.com>
2022-04-09 19:28:47 -0700

    Order countries alphabetically

--- _data/countries.js
+++ _data/countries.js
@@ -5,11 +5,10 @@ const client = contentful.createClient({
 });
 
 module.exports = function () {
-  return client.getEntries({ content_type: 'country', order: 'sys.createdAt' })
+  return client.getEntries({ content_type: 'country', order: 'fields.countryName' })
     .then(function (response) {
       const country = response.items
         .map(function (country) {
-          country.fields.date = new Date(country.sys.updatedAt);
           return country.fields;
         });
       return country;
