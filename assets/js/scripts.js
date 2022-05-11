ce3dc29440c3bcd9d5a5b0eca86428df06990606
Melanie Richards <mr@melanie-richards.com>
2022-05-10 19:10:40 -0700

    Support light/dark mode with theme switcher

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,45 @@
 if (navigator.platform.indexOf('Win') === 0) {
   document.documentElement.setAttribute('data-is-win', 'true');
 }
+
+// Enable user selection of color scheme
+
+  // Check for stored preference
+  const themePrefKey = 'theme-pref';
+
+  const getStoredThemePref = function() {
+    const storedThemePref = localStorage.getItem(themePrefKey);
+    if (storedThemePref) {
+      return storedThemePref;
+    } else {
+      return 'system';
+    }
+  };
+
+  // Apply stored preference
+  const storedThemePref = getStoredThemePref();
+  document.documentElement.setAttribute('data-prefers-theme', storedThemePref);
+
+  // Set new preference
+  const setThemePref = function(themeValue) {
+    document.documentElement.setAttribute('data-prefers-theme', themeValue);
+    localStorage.setItem(themePrefKey, themeValue);
+  };
+
+  // Wait until document is parsed
+  window.onload = function() {
+
+    // Check radio according to stored pref
+    document.getElementById('theme-radio-' + storedThemePref).setAttribute('checked', '');
+
+    // Switch preference
+    const themeRadios = document.querySelectorAll('[name=theme]');
+    themeRadios.forEach(function(themeRadio) {
+      themeRadio.addEventListener("change", function() {
+        if (themeRadio.checked) {
+          const themeValue = themeRadio.getAttribute('value');
+          setThemePref(themeValue);
+        }
+      });
+    });
+  };
